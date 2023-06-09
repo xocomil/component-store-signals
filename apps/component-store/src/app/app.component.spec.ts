8d917c86a8ac266e07d5f77523f2ea4b8e4eab47
xocomil <xocomil@gmail.com>
2023-06-08 18:38:33 -0600

    converted to `@ng-neat/spectator`

--- apps/component-store/src/app/app.component.spec.ts
+++ apps/component-store/src/app/app.component.spec.ts
@@ -1,26 +1,23 @@
-import { TestBed } from '@angular/core/testing';
+import { createComponentFactory } from '@ngneat/spectator/jest';
+import { MockComponent } from 'ng-mocks';
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { PersonDisplayComponent } from './components/person-display/person-display.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AppComponent, NxWelcomeComponent],
-    }).compileComponents();
+  const createComponent = createComponentFactory({
+    component: AppComponent,
+    overrideComponents: [
+      [
+        AppComponent,
+        {
+          remove: { imports: [PersonDisplayComponent] },
+          add: { imports: [MockComponent(PersonDisplayComponent)] },
+        },
+      ],
+    ],
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome component-store'
-    );
-  });
-
-  it(`should have as title 'component-store'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('component-store');
+  it('should create', () => {
+    expect(createComponent()).toBeDefined();
   });
 });
